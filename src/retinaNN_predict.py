8ee771688d74285a48f5e84fb5c76b030e4c88b9
Granbe1l <candyristyan1@gmail.com>
2025-10-29 12:10:10 +0700

    chebyshev

--- src/retinaNN_predict.py
+++ src/retinaNN_predict.py
@@ -136,6 +136,10 @@ for i in range(Imgs_to_test):
 
     pred_patches = pred_to_imgs(prediction, patch_height, patch_width, "original") # Kembali ke 4D channels_first
 
+    # ===================================================================
+    # BARIS transpose YANG SALAH SUDAH DIHAPUS DARI SINI
+    # ===================================================================
+
     # recompone_overlap asli mengharapkan channels_first
     pred_img = recompone_overlap(pred_patches, new_height, new_width, stride_height, stride_width)
 
